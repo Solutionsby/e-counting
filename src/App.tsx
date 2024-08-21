416ada53d82a68902f6c0e9c08a4bc3ac9f0ac70
Pawel Goc <pawel.goc@solutionsby.pl>
2024-08-21 20:38:01 +0200

    Update hero section copy to enhance trust and engagement

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./App.css";
 import { Header } from "./components/header/Header";
 import { ScrollPrvider } from "./ScrollProvider";
 import { Form } from "./components/form/Form";
+import { HeroSection } from "./components/hero/HeroSection";
 
 function App() {
 	const formReference = useRef<HTMLDivElement>(null);
 	return (
 		<ScrollPrvider>
 			<Header formReference={formReference} />
+			<HeroSection />
 			<Form formReference={formReference} />
 		</ScrollPrvider>
 	);
