a58163071583cbf36484b9a97df2e3f194bb1416
Pawel Goc <pawel.goc@solutionsby.pl>
2024-08-25 20:21:53 +0200

    services section

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -3,7 +3,6 @@ import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { Button } from "../button/Button";
 import { services } from "../../db/services.json";
 
-
 interface ServProps {
 	icon: string;
 	title: string;
@@ -14,7 +13,6 @@ interface ServicesProps {
 }
 
 export const Services: React.FC<ServicesProps> = ({ formReference }) => {
-
 	return (
 		<div className=" bg-primary text-complWhite h-auto w-full flex flex-col">
 			<div className="flex flex-col items-center">
@@ -24,11 +22,11 @@ export const Services: React.FC<ServicesProps> = ({ formReference }) => {
 					gospodarczą. Oferujemy wsparcie dla:
 				</p>
 			</div>
-			<div className="flex flex-col items-center">
+			<div className="flex flex-col items-center md:flex-row md:justify-around">
 				{services.map(({ icon, title, description }: ServProps, index) => (
 					<div
 						key={index}
-						className="w-[85%] flex  flex-col items-center p-2 border-2 rounded-md mt-5 mb-5 gap-2 bg-secendary">
+						className="w-[85%] flex  flex-col items-center p-2 border-2 rounded-md mt-5 mb-5 gap-2 bg-secendary md:w-[25%] md:h-[300px] md:justify-center">
 						<FontAwesomeIcon icon={icon as IconProp} className="text-5xl" />
 						<h1 className="text-2xl text-center">{title}</h1>
 						<p className="w-[70%] text-center">{description}</p>
@@ -37,7 +35,7 @@ export const Services: React.FC<ServicesProps> = ({ formReference }) => {
 			</div>
 			<Button
 				reference={formReference}
-				className="flex items-center justify-center w-[60%] mx-auto h-auto p-3 m-5 bg-secendary border-complWhite border-2 rounded-md text-primary font-bold hover:bg-white hover:text-primary transition duration-500">
+				className="flex items-center justify-center w-[60%] mx-auto h-auto p-3 m-5 bg-secendary border-complWhite border-2 rounded-md text-primary font-bold hover:bg-white hover:text-primary transition duration-500 md:w-[25%] md:p-10">
 				CHCĘ SKORZYSTAĆ Z WASZEJ WIEDZY
 			</Button>
 		</div>
